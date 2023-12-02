bfec9504a91a65b784128f6699153216a0d6a68e
Pedro H G Bonel <pedro.bonel@gmail.com>
2023-12-01 23:31:48 -0300

    [ADD] Interfaces

---
+++ backend/src/interfaces.ts
@@ -0,0 +1,16 @@
+import mongoose, { Types } from "mongoose";
+
+export interface IMessage {
+  _id: mongoose.ObjectId
+  sender: string
+  text: string
+  media?: string
+}
+
+export interface IConversation {
+  _id: mongoose.ObjectId
+  code: string
+  messages: Types.DocumentArray<IMessage>
+  createdAt: Date
+  updatedAt: Date
+}
