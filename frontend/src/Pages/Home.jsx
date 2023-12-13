db908ef055237c425bc24d97e5f7ae38a10fd2b3
Pedro H G Bonel <pedro.bonel@gmail.com>
2023-12-13 00:40:03 -0300

    [ADD] Interactivity on home page

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function Home() {
+  useEffect(() => {
+    let cursorTimeout;
+
+    const buttons = document.getElementsByClassName('home__button');
+    if (buttons.length > 0) {
+      buttons[0].focus();
+    }
+
+    document.addEventListener('mousemove', function () {
+      document.body.style.cursor = 'auto';
+
+      cursorTimeout = setTimeout(() => document.body.style.cursor = 'none', 4000);
+    })
+
+    document.addEventListener('keydown', function (event) {
+      switch (event.key) {
+        case 'h':
+        case 'ArrowLeft':
+          return document.getElementById('join').focus();
+        case 'l':
+        case 'ArrowRight':
+          return document.getElementById('create').focus();
+      }
+    })
+
+    return () => {
+      clearTimeout(cursorTimeout);
+    }
+  }, [])
+
   return (
-    <div>Home</div>
+    <main className='home'>
+      <h1 className='home__title'>Chit-Chat</h1>
+      <h4 className='home__subtitle'>Talk about anything. Anonymously.</h4>
+      <div className='home__button-container'>
+        <button className='home__button' id='join'>Join chat</button>
+        <button className='home__button' id='create'>Create chat</button>
+      </div>
+    </main>
   )
 }
 
