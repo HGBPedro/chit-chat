f0dd72a84cbf166bcb4868310a0e2161c9484b27
Pedro H G Bonel <pedro.bonel@gmail.com>
2024-01-08 00:18:16 -0300

    [ADD] Constants files of refactor

---
+++ frontend/src/constants.jsx
@@ -0,0 +1,10 @@
+const HOME_TEXTS = {
+  title: <span>This is <b>chit-chat</b></span>,
+  subtitle: <span>A place where you can talk <strong>anonymously about anything.</strong></span>,
+  description: <span>First you must create or join a chat. You can type <em>help</em> to see how to get started.</span>
+}
+
+const HOME_COMMANDS = ['set-username']
+
+
+export { HOME_TEXTS }
