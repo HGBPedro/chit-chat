36fc9e39c905be138a0c09e2ac7cb881f005eda7
Pedro H G Bonel <pedro.bonel@gmail.com>
2024-01-13 12:44:25 -0300

    [ADD] Some terminal commands

--- frontend/src/constants.jsx
+++ frontend/src/constants.jsx
@@ -4,7 +4,17 @@ const HOME_TEXTS = {
   description: <span>First you must create or join a chat. You can type <em>help</em> to see how to get started.</span>
 }
 
-const HOME_COMMANDS = ['set-username']
+const HELP_COMMAND = (
+  <span>
+    <p>usage: &lt;command&gt; [&lt;args&gt;]</p>
+    <br/>
+    <p>Here are the commands to be used:</p>
+    <br/>
+    <pre>join-chat &lt;hash&gt; [-nickname &lt;name&gt;]  join a chat using the chat code</pre>
+    <pre>create-chat [-nickname &lt;name&gt;]       create a chat</pre>
+    <pre>set-nickname &lt;nickname&gt;              set the nickname used in the chat</pre>
+    <pre>clear                                clear the terminal</pre>
+  </span>
+)
 
-
-export { HOME_TEXTS }
+export { HOME_TEXTS, HELP_COMMAND }
